7f911d31eb3ca9ae199530ab43920414a06babbe
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-12-10 14:10:40 +0900

    first commit

---
+++ src/main/kotlin/com/yamachoo/Application.kt
@@ -0,0 +1,12 @@
+package com.yamachoo
+
+import io.ktor.server.application.*
+import com.yamachoo.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
