29a334689592274ebbc3cd62f071b649a2c02954
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-12-11 17:11:36 +0900

    add: /graphql

--- src/main/kotlin/com/yamachoo/plugins/Routing.kt
+++ src/main/kotlin/com/yamachoo/plugins/Routing.kt
@@ -1,16 +1,19 @@
 package com.yamachoo.plugins
 
+import com.yamachoo.graphql.server.KtorGraphQLServer
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        post("/graphql") {
+            KtorGraphQLServer.handle(this.call)
         }
     }
 }
