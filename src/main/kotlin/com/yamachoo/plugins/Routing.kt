04898192d21146fe66fdd331c5786aca3c800189
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-12-11 17:42:53 +0900

    add: playground

--- src/main/kotlin/com/yamachoo/plugins/Routing.kt
+++ src/main/kotlin/com/yamachoo/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package com.yamachoo.plugins
 
 import com.yamachoo.graphql.server.KtorGraphQLServer
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -15,5 +16,15 @@ fun Application.configureRouting() {
         post("/graphql") {
             KtorGraphQLServer.handle(this.call)
         }
+
+        get("/playground") {
+            this.call.respondText(buildPlaygroundHtml("graphql", "subscriptions"), ContentType.Text.Html)
+        }
     }
 }
+
+private fun buildPlaygroundHtml(graphQLEndpoint: String, subscriptionsEndpoint: String) =
+    Application::class.java.classLoader.getResource("graphql-playground.html")?.readText()
+        ?.replace("\${graphQLEndpoint}", graphQLEndpoint)
+        ?.replace("\${subscriptionsEndpoint}", subscriptionsEndpoint)
+        ?: throw IllegalStateException("graphql-playground.html cannot be found in the classpath")
