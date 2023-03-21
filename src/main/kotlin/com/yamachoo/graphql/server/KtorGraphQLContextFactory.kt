c266a7e28450c592a25c84b7e5f65d77fbdc3b51
yamachoo <51016095+yamachoo@users.noreply.github.com>
2023-03-22 08:22:22 +0900

    remove: old graphql implementation

--- src/main/kotlin/com/yamachoo/graphql/server/KtorGraphQLContextFactory.kt
+++
@@ -1,6 +0,0 @@
-package com.yamachoo.graphql.server
-
-import com.expediagroup.graphql.server.execution.GraphQLContextFactory
-import io.ktor.server.request.*
-
-class KtorGraphQLContextFactory : GraphQLContextFactory<ApplicationRequest>
--- src/main/kotlin/com/yamachoo/plugins/Routing.kt
+++ src/main/kotlin/com/yamachoo/plugins/Routing.kt
@@ -1,7 +1,5 @@
 package com.yamachoo.plugins
 
-import com.yamachoo.graphql.server.KtorGraphQLServer
-import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -12,19 +10,5 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
-
-        post("/graphql") {
-            KtorGraphQLServer.handle(this.call)
-        }
-
-        get("/playground") {
-            this.call.respondText(buildPlaygroundHtml("graphql", "subscriptions"), ContentType.Text.Html)
-        }
     }
 }
-
-private fun buildPlaygroundHtml(graphQLEndpoint: String, subscriptionsEndpoint: String) =
-    Application::class.java.classLoader.getResource("graphql-playground.html")?.readText()
-        ?.replace("\${graphQLEndpoint}", graphQLEndpoint)
-        ?.replace("\${subscriptionsEndpoint}", subscriptionsEndpoint)
-        ?: throw IllegalStateException("graphql-playground.html cannot be found in the classpath")
